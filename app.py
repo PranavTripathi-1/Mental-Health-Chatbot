f2c11965030c9c3c6855442faf2816c78ea86c2e
PranavTripathi-1 <pranavtripathi2005gopal@gmail.com>
2025-05-19 17:40:48 +0530

    Update app.py

--- app.py
+++ app.py
@@ -29,7 +29,7 @@ def chat():
 
     return {'reply': bot_reply}
 
-if __name__ == '__main__':
+#if __name__ == '__main__':
     app.run(debug=True)
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=10000)
